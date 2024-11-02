6216adebad56f721614002acef92545e232c954b
sudipmanjarekar <manjarekarsudip@gmail.com>
2024-11-02 13:42:50 +0530

    CircularSentence

--- src/DeleteCharacterstoMakeFancyString.java
+++ src/DeleteCharacterstoMakeFancyString.java
@@ -1,4 +1,48 @@
+import java.util.Iterator;
 
 public class DeleteCharacterstoMakeFancyString {
 
+	/*
+	 * A fancy string is a string where no three consecutive characters are equal.
+	 * 
+	 * Given a string s, delete the minimum possible number of characters from s to
+	 * make it fancy.
+	 * 
+	 * Return the final string after the deletion. It can be shown that the answer
+	 * will always be unique.
+	 * 
+	 * 
+	 * 
+	 * Example 1:
+	 * 
+	 * Input: s = "leeetcode" Output: "leetcode" Explanation: Remove an 'e' from the
+	 * first group of 'e's to create "leetcode". No three consecutive characters are
+	 * equal, so return "leetcode". Example 2:
+	 * 
+	 * Input: s = "aaabaaaa" Output: "aabaa" Explanation: Remove an 'a' from the
+	 * first group of 'a's to create "aabaaaa". Remove two 'a's from the second
+	 * group of 'a's to create "aabaa". No three consecutive characters are equal,
+	 * so return "aabaa". Example 3:
+	 * 
+	 * Input: s = "aab" Output: "aab" Explanation: No three consecutive characters
+	 * are equal, so return "aab".
+	 */
+
+	public String makeFancyString(String s) {
+
+		if (s.length() < 3) {
+			return s;
+		}
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(s.charAt(0)).append(s.charAt(1));
+
+		for (int i = 2; i < s.length(); i++) {
+			if (s.charAt(i) != sb.charAt(sb.length() - 1) || s.charAt(i) != sb.charAt(sb.length() - 2)) {
+				sb.append(s.charAt(i));
+			}
+		}
+
+		return sb.toString();
+	}
 }
